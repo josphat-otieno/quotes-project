9e809de1c9f64895e154c3c6373b68d17a67d194
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-05-16 15:59:13 +0300

    define variables for upvote and downvote

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,7 +3,7 @@ export class Quote {
     // author!:string
     // quote!:string
     showQuote:boolean;
-    constructor(public name:string, public author:string, public quote:string, public postDate:Date ){
+    constructor(public name:string, public author:string, public quote:string, public postDate:Date, public upvote:number, public downvote:number){
         this.showQuote=false
     }
 
